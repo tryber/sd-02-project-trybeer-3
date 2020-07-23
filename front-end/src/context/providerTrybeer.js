77fa778dd0cb3ecaf03f66b589aad0deccf2f0a2
Giorgia <giorgialeoniac@gmail.com>
2020-07-23 17:36:24 -0300

    Primeiras congis Gi+Folgado

---
+++ front-end/src/context/providerTrybeer.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Trybeer from './index';
+
+const ProviderTrybeer = ({ children }) => {
+  const [teste, setTeste] = useState("jorjao");
+  const toProvider = {
+    teste, 
+    setTeste
+  };
+  return (
+    <Trybeer.Provider value={toProvider}>
+      { children }
+    </Trybeer.Provider>
+  );
+}
+
+export default ProviderTrybeer;
+
+ProviderTrybeer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
