bd0ff9c80791f7ee999be2afbb795f217b117a1b
Giorgia <giorgialeoniac@gmail.com>
2020-07-28 19:36:16 -0300

    Ligadão com backend

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -3,9 +3,6 @@ import { Link } from 'react-router-dom';
 import { postLogin } from '../services';
 import '../styles/Login.css';
 
-const [email, setEmail] = useState('');
-const [senha, setSenha] = useState('');
-
 const btnSemConta = () => (
   <Link to="/register">
     <div className="btn-sem-conta-div">
@@ -19,44 +16,48 @@ const btnSemConta = () => (
   </Link>
 );
 
-const renderLoginSection = () => (
+const renderLoginSection = (email, setEmail, senha, setSenha) => (
   <div className="logincampo">
-    <label htmlFor="email">E-mail:</label>
-    <input
-      id="email"
-      name="email"
-      type="text"
-      data-testid="email-input"
-      value={email}
-      onChange={(event) => setEmail(event.target.value)}
-    />
-    <label htmlFor="name">Senha:</label>
-    <input
-      id="senha"
-      name="senha"
-      type="password"
-      data-testid="password-input"
-      value={senha}
-      onChange={(event) => setSenha(event.target.value)}
-    />
+    <form>
+      <label htmlFor="email">E-mail:</label>
+      <input
+        id="email"
+        name="email"
+        type="text"
+        data-testid="email-input"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <label htmlFor="name">Senha:</label>
+      <input
+        id="senha"
+        name="senha"
+        type="password"
+        data-testid="password-input"
+        value={senha}
+        onChange={(event) => setSenha(event.target.value)}
+      />
+    </form>
   </div>
 );
 
-const loginClick = async () => {
-  const data = postLogin({ email, password: senha })
+const loginClick = async (email, senha) => {
+  const data = await postLogin({ email, password: senha })
     .then((response) => response.data);
   console.log(data);
 };
 
 export default function Login() {
   const [disabled, setDisabled] = useState(true);
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
 
   useEffect(() => {
     const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (senha.length >= 6 && regexEmail.test(email)) {
       return setDisabled(false);
     }
-    return setDisabled(true);
+    setDisabled(true);
   }, [email, senha]);
 
   const renderLoginButton = () => (
@@ -67,7 +68,7 @@ export default function Login() {
           className="btn-login"
           data-testid="signin-btn"
           disabled={disabled}
-          onClick={loginClick()}
+          onClick={() => loginClick(email, senha)}
         >
           ENTRAR
         </button>
@@ -77,8 +78,8 @@ export default function Login() {
 
   return (
     <div>
-      {renderLoginSection()}
-      {renderLoginButton()}
+      {renderLoginSection(email, setEmail, senha, setSenha)}
+      {renderLoginButton(disabled, setDisabled)}
       {btnSemConta()}
     </div>
   );
