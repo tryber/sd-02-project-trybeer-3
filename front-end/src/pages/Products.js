2de67605ec2bc1a520c50b1d7a1c22f9655c91a9
richardwellerson <richardwellerson@gmail.com>
2020-07-29 16:29:15 -0300

    map in request works fine.

--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import Sidebar from '../components/Sidebar/Sidebar';
+import AllProducts from '../components/Products/AllProducts';
 
 const Products = () => (
   <div>
     <Header />
     <Sidebar />
+    <AllProducts />
   </div>
 );
 
