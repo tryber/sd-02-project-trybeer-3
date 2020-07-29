47003a449fb102897199a4990598a183bbab67dc
phtf92 <phtf92@gmail.com>
2020-07-29 16:21:52 -0300

    Checkout page

--- back-end/controllers/productsController.js
+++ back-end/controllers/productsController.js
@@ -1,4 +1,4 @@
-const { getProducts, newOrder } = require('../services/productsService');
+const { getProducts, newOrder, addProducts } = require('../services/productsService');
 
 const getAllProducts = async (_req, res) => {
   const products = await getProducts();
@@ -8,14 +8,18 @@ const getAllProducts = async (_req, res) => {
   });
 };
 
-const createOrder = async (req, res) => {
-  const { id } = req.user;
-  const { street, streetNumber } = req.body;
-  const createdOrder = await newOrder(id, street, streetNumber);
-  console.log(createdOrder);
-  res.status(200).json({
-    status: 'success',
-  });
+const createOrder = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    const { street, streetNumber, products } = req.body;
+    const createdOrder = await newOrder(id, street, streetNumber, products);
+    const addProductOrder = await addProducts(products, createdOrder);
+    res.status(200).json({
+      status: 'success',
+    });
+  } catch (error) {
+    next({ code: 'something_wrong', message: 'Something went wrong' });
+  }
 };
 
 module.exports = {
--- back-end/models/productsModel.js
+++ back-end/models/productsModel.js
@@ -1,10 +1,17 @@
 const { getAll } = require('./Utils/getFromDB');
+const { insertDb } = require('./Utils/insertDB');
 
 const getAllProducts = async (fields) => {
   const products = await getAll('Products', fields);
   return products.map(([productId, name, price]) => ({ name, productId, price }));
 };
 
+const createNewOrder = async (newOrderFields, params) => insertDb('Orders', newOrderFields, params);
+
+const addToOrder = async (fields, params) => insertDb('Order_Products', fields, params);
+
 module.exports = {
   getAllProducts,
+  createNewOrder,
+  addToOrder,
 };
