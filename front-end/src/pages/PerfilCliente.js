a69c513fdcca55917cc4906ef4c7e7fd4b26af11
Giorgia <giorgialeoniac@gmail.com>
2020-07-30 15:52:49 -0300

    CC

--- front-end/src/pages/PerfilCliente.js
+++ front-end/src/pages/PerfilCliente.js
@@ -51,7 +51,7 @@ const renderSalvarButton = (email, senha, disabled) => (
 export default function Login() {
   const { setPage } = useContext(Trybeer);
   getDados();
-  useEffect(() => { setPage('Meu perfil') }, []);
+  useEffect(() => { setPage('Meu perfil'); }, []);
   // const [newClient, setNewClient] = useState('');
 
   // useEffect(() => {
@@ -66,8 +66,8 @@ export default function Login() {
       <Header />
       <Sidebar />
       <div className="div-page-body-perfil">
-      {renderPerfilSection()}
-      {renderSalvarButton()}
+        {renderPerfilSection()}
+        {renderSalvarButton()}
       </div>
     </div>
   );
