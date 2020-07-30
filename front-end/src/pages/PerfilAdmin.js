ec7ed429e18c2f33305e351ef4f57598c4a149e3
Giorgia <giorgialeoniac@gmail.com>
2020-07-30 17:31:25 -0300

    Front responsivo - apenas estrutura

--- front-end/src/pages/PerfilAdmin.js
+++ front-end/src/pages/PerfilAdmin.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import Trybeer from '../context';
 import Sidebar from '../components/Sidebar/Sidebar';
 import '../styles/PerfilAdmin.css';
 
 export default function PerfilAdmin() {
+  const { setOpen } = useContext(Trybeer);
+  useEffect(() => { setOpen(true); }, []);
+
   const renderPerfilAdmin = () => (
     <article className="page-perfil-admin">
       <h1 className="admin-perfil-title">Perfil</h1>
